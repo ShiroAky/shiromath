8dae0f86f36e1ed00b9946ebf6382826cd822b9e
ShiroAky <73247790+ShiroAky@users.noreply.github.com>
2024-07-20 23:39:01 -0400

    V1.0.0

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Modulo:
+export function Suma(a, b) {
+    return Math.round(a + b);
+}
+
+export function Resta(a, b) {
+    return Math.round(a - b);
+}
+
+export function Multiplicacion(a, b) {
+    return Math.round(a * b);
+}
+
+export function Division(a, b) {
+    return Math.round(a / b);
+}
+
+export function Potencia(a, b) {
+    return Math.round(Math.pow(a, b));
+}
